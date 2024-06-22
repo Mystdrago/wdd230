3dddc7a1104a385cf73354acf7183f496bebc03a
Curtis Halls <curtishalls@ymail.com>
2024-06-22 17:29:32 -0600

    it did not but now it all works ugh

--- chamber/scripts/menu.js
+++ chamber/scripts/menu.js
@@ -1,10 +1,6 @@
-const menuButton = document.getElementById('menu-button');
-const navMenu = document.getElementById('nav-menu');
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('nav ul');
 
-menuButton.addEventListener('click', () => {
-  if (navMenu.style.display === 'none' || navMenu.style.display === '') {
-    navMenu.style.display = 'block';
-  } else {
-    navMenu.style.display = 'none';
-  }
+hamburger.addEventListener('click', () => {
+  navMenu.classList.toggle('show');
 });
